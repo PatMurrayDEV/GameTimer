8d91a813e61adddc29efd91715fa5bf36b9ba6d1
Patrick Murray <patrick@patmurraydev.com>
2015-12-05 22:20:37 +1100

    Add ignore

---
+++ PictionaryTimer/SecondaryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondaryViewController.swift
+//  PictionaryTimer
+//
+//  Created by Patrick Murray on 5/12/2015.
+//  Copyright © 2015 Patrick Murray. All rights reserved.
+//
+
+import UIKit
+
+class SecondaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PictionaryTimer/TimerObject.swift
@@ -0,0 +1,13 @@
+//
+//  TimerObject.swift
+//  PictionaryTimer
+//
+//  Created by Patrick Murray on 5/12/2015.
+//  Copyright © 2015 Patrick Murray. All rights reserved.
+//
+
+import UIKit
+
+class TimerObject: NSObject {
+
+}
